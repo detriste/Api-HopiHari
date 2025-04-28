c713958bbc6fc39e48bb550173e20b9f689b69b8
detriste <detriste2018@gmail.com>
2025-04-28 15:03:06 -0300

    goku

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("goku zika")
+const express = require("express");
+const app = require();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const helmet = require ("helmet")
+
+app.use(cors());
+app.use(helmet());
+
+app.use(bodyParser.urlencoded({extends: false}))
+app.use(bodyParser.json())
