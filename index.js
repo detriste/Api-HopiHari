b9da9576a3c637b5c7430b479e86bc46f9bfc406
detriste <detriste2018@gmail.com>
2025-04-28 15:15:49 -0300

    n

--- index.js
+++ index.js
@@ -9,3 +9,18 @@ app.use(helmet());
 
 app.use(bodyParser.urlencoded({extends: false}))
 app.use(bodyParser.json())
+
+app.use((req,res,next) =>{
+    res.header("Acces-Control-Allow-Origins","*");
+    res.header(
+"Acess-Control-Allow-Header",
+"Origin,X-Requested-With,Content-Type,Accept,Authorization"
+
+    );
+
+    if (req.method === 'OPTIONS'){
+res.header("Access-Control-Allow-Methods","PUT,POST,GET,PATCH,DELETE")
+
+    }
+    next();
+})
