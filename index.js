9b8a32cff4496a5fe31577d462dfb16ef3b9c591
detriste <detriste2018@gmail.com>
2025-04-29 08:42:31 -0300

    c

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const helmet = require ("helmet")
+const helmet = require("helmet");
 const bodyParser = require("body-parser");
 
+const usuariosRoute = require("./routes/login.route")
 
 app.use(cors());
 app.use(helmet());
 
-app.use(bodyParser.urlencoded({extends: false}));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.use((req, res, next) =>{
-    res.header("Acces-Control-Allow-Origins","*");
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origins", "*");
     res.header(
-"Acess-Control-Allow-Header",
-"Origin,X-Requested-With,Content-Type,Accept,Authorization"
-
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
     );
-
-    if (req.method === 'OPTIONS'){
-res.header("Access-Control-Allow-Methods","PUT,POST,GET,PATCH,DELETE")
+    if (req.method === 'OPTIONS') {
+        res.header("Access-Control-Allow-Methods", "PUT, POST, GET, PATCH, DELETE");
 
     }
     next();
-})
+});
+
+app.use("/usuarios", usuariosRoute);
+
+module.exports = app;
