1710543d24cea0d966d30ff4c9d7b457a7a8e2a8
detriste <detriste2018@gmail.com>
2025-05-05 15:37:22 -0300

    x

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.use(helmet());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header(
