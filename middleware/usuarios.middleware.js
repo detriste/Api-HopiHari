a1675be921ccf23091ad435d3a947af9947c005c
detriste <detriste2018@gmail.com>
2025-05-20 10:24:17 -0300

    ç

--- middleware/usuarios.middleware.js
+++ middleware/usuarios.middleware.js
@@ -24,3 +24,15 @@ catch(error){
 }
 
 }
+
+exports.userRequired = async (req,res,next) => {
+    try{
+        if (res.locals.idUsuario == 0) {
+            return res.status(401).send({"mensagem":"usuario não auorizado "});
+        }
+        next();
+    }
+    catch(error){
+        return res.status(500).send(error);
+    }
+}
