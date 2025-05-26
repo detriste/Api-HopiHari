2027409f284a05f96cb2d9ddea44e2fa302de49c
detriste <detriste2018@gmail.com>
2025-05-26 08:15:04 -0300

    s

--- controller/brinquedos.controller.js
+++ controller/brinquedos.controller.js
@@ -4,8 +4,12 @@ const mysql = require('../routes/mysql');
 exports.cadastroBrinquedo = async (req, res) => {
     try {
       console.log('Dados recebidos:', req.body);
-      const resultados = await mysql.execute(`INSERT INTO atracao (nome, tempo_espera, status, area) VALUES (?, ?, ?, ?)`, 
-        [req.body.nome, req.body.tempo_espera, req.body.status, req.body.area]);
+      const resultados = await mysql.execute(`INSERT INTO atracoes (nome, tempo_espera, status, area) VALUES (?, ?, ?, ?)`, 
+        [   req.body.nome, 
+          req.body.tempo_espera, 
+          req.body.status, 
+          req.body.area  ]);
+
       console.log('Resultados da query:', resultados);
       return res.status(201).send({
         mensagem: "Brinquedo cadastrado com sucesso",
